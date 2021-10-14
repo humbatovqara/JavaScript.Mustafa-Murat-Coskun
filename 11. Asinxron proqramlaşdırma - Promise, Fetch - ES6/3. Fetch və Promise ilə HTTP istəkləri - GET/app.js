f0be5d58b73b0451e9f804c3c300dfb832f2364c
humbatovqara <qarahumbtov@gmail.com>
2021-10-14 15:14:14 +0400

    Update: HTTP Request POST with Fetch

--- 11. Asinxron proqramlaşdırma - Promise, Fetch - ES6/3. Fetch və Promise ilə HTTP istəkləri - GET/app.js
+++ 11. Asinxron proqramlaşdırma - Promise, Fetch - ES6/3. Fetch və Promise ilə HTTP istəkləri - GET/app.js
@@ -12,16 +12,46 @@ class Request {
                 .catch(err => reject(err));
         });
     }
+
+    // POST Request
+    post(url, data) {
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        title: 'foo',
+                        body: 'bar',
+                        userId: 1
+                    }),
+                    headers: {
+                        "Constent-type": "application/json; charset=UTF-8"
+                    }
+                })
+                .then(response => response.json())
+                .then(data => resolve(data))
+                .catch(err => reject(err))
+        })
+
+    }
 }
 
 const request = new Request();
-let albums;
+// let albums;
 
+/*
 request.get("https://jsonplaceholder.typicode.com/albums")
     .then(albums => {
         console.log(albums);
     })
     .catch(err => console.log(err));
+*/
 
 // Asenxron
 // console.log(albums);
+
+request.post("https://jsonplaceholder.typicode.com/albums", {
+        uderId: 1,
+        title: "Test"
+    })
+    .then(newAlbum => console.log(newAlbum))
+    .catch(err => console.log(err));
